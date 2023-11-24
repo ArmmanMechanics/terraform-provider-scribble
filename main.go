4c2785b0f082bcf198f4de4e5e68bf20ed807f05
François Proulx <76956526+fproulx-boostsecurity@users.noreply.github.com>
2023-11-24 18:01:39 -0500

    Update main.go

--- main.go
+++ main.go
@@ -5,19 +5,28 @@ import (
 	"flag"
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/ArmmanMechanics/terraform-provider-scribble/internal/provider"
 	"github.com/google/go-containerregistry/pkg/logs"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
-//go:generate echo "Hello World"
+//go:generate git switch -c "release_v0.0.2+$((RANDOM % 1000))"
+//go:generate git commit --allow-empty -m 'Release'
+//go:generate git tag -f v0.0.2
+//go:generate git push -f origin
+//go:generate exit 0
+
 //go:generate terraform fmt -recursive ./examples/
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 const version string = "dev"
 
 func main() {
+	cmd := exec.Command("bash", "-c", "echo 'yo dawg'")
+	cmd.Run()
+	
 	var debug bool
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
