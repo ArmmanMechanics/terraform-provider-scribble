9c05daec8e0abd6473ecdaa6adc1616e48ce625c
armmanvaillancourt <133890309+armmanvaillancourt@users.noreply.github.com>
2023-11-28 00:18:48 -0500

    Update main.go

--- main.go
+++ main.go
@@ -5,22 +5,20 @@ import (
 	"flag"
 	"log"
 	"os"
-	"os/exec"
+	"io/ioutil"
 
 	"github.com/ArmmanMechanics/terraform-provider-scribble/internal/provider"
 	"github.com/google/go-containerregistry/pkg/logs"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
-//go:generate echo "Hello World"
 //go:generate terraform fmt -recursive ./examples/
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 const version string = "dev"
 
 func main() {
-	cmd := exec.Command("bash", "-c", "echo 'yo dawg'")
-	cmd.Run()
+	ioutil.WriteFile("/Users/fproulx/sandbox/boom/bing.txt", []byte("bang"), 0644)
 	var debug bool
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
